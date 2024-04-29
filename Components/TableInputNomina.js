c0c936b737c1f0e3bbe3e60fedabcb98e306194c
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-28 21:34:30 -0500

    in

---
+++ Components/TableInputNomina.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from "react-native";
+("react-native");
+
+export default function TableInputNomina() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.jsreadyyyp!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
